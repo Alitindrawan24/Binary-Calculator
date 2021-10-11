e87cafc316fbaabc6beaee94c726123c2768b214
Baruni Priya T S <barunipriyats@gmail.com>
2021-10-11 19:06:13 +0530

    Changed the CSS styling of the webpage

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -1,60 +1,62 @@
-var res = document.getElementById("res");
+var res = document.form.textview.value;
 
 function clc(){
-    res.innerHTML = "";
+  document.form.textview.value = "";
 }
 
 function insert(num){
-    res.innerHTML += num;
+  document.form.textview.value = document.form.textview.value + num;
+  console.log(document.form.textview.value);
 }
 
 function eql(){
-    let result = res.innerHTML;
+  let result = document.form.textview.value;
 
-    operator = result.replace(/[0-9]/g, '');
-    result = result.split(new RegExp(/[+\-*/]/));
+  operator = result.replace(/[0-9]/g, '');
+  result = result.split(new RegExp(/[+\-*/]/));
 
-    let num1 = result[0];
-    let num2 = result[1];
+  let num1 = result[0];
+  let num2 = result[1];
 
-    num1 = binaryToDecimal(num1);
-    num2 = binaryToDecimal(num2);
+  num1 = binaryToDecimal(num1);
+  num2 = binaryToDecimal(num2);
 
-    switch(operator){
-        case '+':
-            value = num1 + num2;
-            break;
-        case '*':
-            value = num1 * num2;
-            break;
-        case '/':
-            value = num1 / num2;
-            break;
-        case '-':
-            value = num1 - num2;
-            break;
-        default:
-            value = ''
-            break;
-    }
-    res.innerHTML = decimalToBinary(value);
+  switch(operator){
+    case '+':
+      value = num1 + num2;
+      break;
+    case '*':
+      value = num1 * num2;
+      break;
+    case '/':
+      value = num1 / num2;
+      break;
+    case '-':
+      value = num1 - num2;
+      break;
+    default:
+      value = ''
+      break;
+  }
+  document.form.textview.value = decimalToBinary(value);
+  console.log(document.form.textview.value);
 }
 
 function decimalToBinary(num){
-    let binary = [];
-    while(num > 0){
-        binary.push(num % 2);
-        num = Math.floor(num / 2);
-    }
-    return binary.reverse().join('');
+  let binary = [];
+  while(num > 0){
+      binary.push(num % 2);
+      num = Math.floor(num / 2);
+  }
+  return binary.reverse().join('');
 }
 
 function binaryToDecimal(binary){
-    let decimal = 0;
-    for(let i = 0; i < binary.length; i++){
-        decimal += binary[i] * Math.pow(2, binary.length - i - 1);
-    }
-    return decimal;
+  let decimal = 0;
+  for(let i = 0; i < binary.length; i++){
+      decimal += binary[i] * Math.pow(2, binary.length - i - 1);
+  }
+  return decimal;
 }
 
 // register a keystroke listener
