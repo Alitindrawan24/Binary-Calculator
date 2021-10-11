def36043df261797f024a238c6d5d2b0a2623bb5
DheekshaGopika <dheekshagopikas@gmail.com>
2021-10-11 13:17:40 +0000

    Refined keydown event code

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -1,14 +1,14 @@
-var res = document.getElementById("res");
+const res = document.getElementById("res");
 
-function clc(){
+function clc() {
     res.innerHTML = "";
 }
 
-function insert(num){
+function insert(num) {
     res.innerHTML += num;
 }
 
-function eql(){
+function eql() {
     let result = res.innerHTML;
 
     operator = result.replace(/[0-9]/g, '');
@@ -20,7 +20,7 @@ function eql(){
     num1 = binaryToDecimal(num1);
     num2 = binaryToDecimal(num2);
 
-    switch(operator){
+    switch(operator) {
         case '+':
             value = num1 + num2;
             break;
@@ -40,42 +40,38 @@ function eql(){
     res.innerHTML = decimalToBinary(value);
 }
 
-function decimalToBinary(num){
+function decimalToBinary(num) {
     let binary = [];
-    while(num > 0){
+    while(num > 0) {
         binary.push(num % 2);
         num = Math.floor(num / 2);
     }
     return binary.reverse().join('');
 }
 
-function binaryToDecimal(binary){
+function binaryToDecimal(binary) {
     let decimal = 0;
-    for(let i = 0; i < binary.length; i++){
+    for(let i = 0; i < binary.length; i++) {
         decimal += binary[i] * Math.pow(2, binary.length - i - 1);
     }
     return decimal;
 }
 
 // register a keystroke listener
-document.addEventListener('keydown', (event) => {
-  if (event.which == 96) { // if numpad0
+document.addEventListener('keydown', event => {
+  if (event.code === "Numpad0" || event.code === "Digit0") {
     insert(0);
-  } else if (event.which == 97) { // if numpad1
+  } else if (event.code === "Numpad1" || event.code === "Digit1") {
     insert(1);
-  } else if (event.which == 48) { // if digit0
-    insert(0);
-  } else if (event.which == 49) { // if digit1
-    insert(1);
-  } else if (event.which == 107) { // if numpadAdd
+  } else if (event.code == "NumpadAdd") {
     insert('+');
-  } else if (event.which == 109) { // if numpadSubtract
+  } else if (event.code == "NumpadSubtract") {
     insert('-');
-  } else if (event.which == 111) { // if numpadDivide
+  } else if (event.code == "NumpadDivide") {
     insert('/');
-  } else if (event.which == 106) { // if numpadMultiply
+  } else if (event.code == "NumpadMultiply") {
     insert('*');
-  } else if (event.which == 13) { // if numpadEnter
+  } else if (event.code == "Enter") {
     eql();
   }
 });
