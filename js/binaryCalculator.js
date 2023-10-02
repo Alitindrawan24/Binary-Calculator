f4b68c0fd1a42b54026107fb305f62f9d1b3b4d1
Bharath-KumarReddy <bk1726797@gmail.com>
2023-10-02 17:38:50 +0530

    Complete Binary_Calculator

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -1,74 +1,84 @@
 const res = document.form.textview;
 
 function clc() {
-    res.value = "";
+  res.value = "";
 }
 
 function insert(num) {
-    res.value += num;
+  res.value += num;
 }
 
 function eql() {
-    let result = res.value;
+  let result = res.value;
 
-    
-    const parts = result.split(/([\+\-\*\/&|^])/).filter(part => part.trim() !== '');
+  const parts = result
+    .split(/([\+\-\*\/&|^><])/)
+    .filter((part) => part.trim() !== "");
 
-    if (parts.length !== 3) {
-        res.value = "Error"; 
+  if (parts.length !== 3) {
+    res.value = "Invalid Input";
+    return;
+  }
+
+  const num1 = parseInt(parts[0], 2);
+  const operator = parts[1];
+  const num2 = parseInt(parts[2], 2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    res.value = "Invalid Input";
+    return;
+  }
+
+  let resultValue;
+
+  switch (operator) {
+    case "+":
+      resultValue = num1 + num2;
+      break;
+    case "-":
+      resultValue = num1 - num2;
+      break;
+    case "*":
+      resultValue = num1 * num2;
+      break;
+    case "/":
+      if (num2 === 0) {
+        res.value = "Undefined";
         return;
-    }
+      }
+      resultValue = num1 / num2;
+      break;
+    case "&":
+      resultValue = num1 & num2; 
+      break;
+    case "|":
+      resultValue = num1 | num2; 
+      break;
+    case "^":
+      resultValue = num1 ^ num2; 
+      break;
+    case ">":
+      resultValue = num1 >> num2;
+      break;
+    case "<":
+      resultValue = num1 << num2;
+      break;
+    default:
+      res.value = "Error";
+      return;
+  }
 
-    const num1 = parseInt(parts[0], 2);
-    const operator = parts[1];
-    const num2 = parseInt(parts[2], 2);
-
-    if (isNaN(num1) || isNaN(num2)) {
-        res.value = "Error"; 
-        return;
-    }
-
-    let resultValue;
-
-    switch (operator) {
-        case '+':
-            resultValue = num1 + num2;
-            break;
-        case '-':
-            resultValue = num1 - num2;
-            break;
-        case '*':
-            resultValue = num1 * num2;
-            break;
-        case '/':
-            if (num2 === 0) {
-                res.value = "Error"; 
-                return;
-            }
-            resultValue = num1 / num2;
-            break;
-        case '&':
-            resultValue = num1 & num2; 
-            break;
-        case '|':
-            resultValue = num1 | num2; 
-            break;
-        case '^':
-            resultValue = num1 ^ num2; 
-            break;
-        default:
-            res.value = "Error"; 
-            return;
-    }
-
-    res.value = decimalToBinary(resultValue);
+  res.value = decimalToBinary(resultValue);
 }
 
 function decimalToBinary(num) {
-    let binary = [];
-    while (num > 0) {
-        binary.push(num % 2);
-        num = Math.floor(num / 2);
-    }
-    return binary.reverse().join('');
+  if (num === 0) {
+    return "0";
+  }
+  let binary = [];
+  while (num > 0) {
+    binary.push(num % 2);
+    num = Math.floor(num / 2);
+  }
+  return binary.reverse().join("");
 }
