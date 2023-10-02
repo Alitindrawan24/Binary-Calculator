43f69c70d76a7fde3461f06a0c467bd09e239789
Clou-D-Walker <92152253+Clou-D-Walker@users.noreply.github.com>
2023-10-02 15:31:54 +0530

    Updated js file and added the shift operators. (#23)

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -1,79 +1,74 @@
-const res = document.form.textview.value;
+const res = document.form.textview;
 
 function clc() {
-    document.form.textview.value = "";
+  res.value = "";
 }
 
 function insert(num) {
-    document.form.textview.value += num;
+  res.value += num;
 }
 
 function eql() {
-    let result = document.form.textview.value;
+  let result = res.value;
 
-    operator = result.replace(/[0-9]/g, '');
-    result = result.split(new RegExp(/[+\-*/]/));
+  const parts = result
+    .split(/([\+\-\*\/&|^><])/)
+    // I have added & | ^ , right shift and left shift operators splitting part too
+    // > and < represent the right and left shift operators respectively
+    .filter((part) => part.trim() !== "");
 
-    let num1 = result[0];
-    let num2 = result[1];
+  if (parts.length !== 3) {
+    res.value = "Error";
+    return;
+  }
 
-    num1 = binaryToDecimal(num1);
-    num2 = binaryToDecimal(num2);
+  const num1 = parseInt(parts[0], 2);
+  const operator = parts[1];
+  const num2 = parseInt(parts[2], 2);
 
-    switch (operator) {
-        case '+':
-            value = num1 + num2;
-            break;
-        case '*':
-            value = num1 * num2;
-            break;
-        case '/':
-            value = num1 / num2;
-            break;
-        case '-':
-            value = num1 - num2;
-            break;
-        default:
-            value = ''
-            break;
-    }
-    document.form.textview.value = decimalToBinary(value);
+  if (isNaN(num1) || isNaN(num2)) {
+    res.value = "Error";
+    return;
+  }
+
+  let resultValue;
+
+  switch (operator) {
+    case "+":
+      resultValue = num1 + num2;
+      break;
+    case "-":
+      resultValue = num1 - num2;
+      break;
+    case "*":
+      resultValue = num1 * num2;
+      break;
+    case "/":
+      if (num2 === 0) {
+        res.value = "Error";
+        return;
+      }
+      resultValue = num1 / num2;
+      break;
+    case ">":
+      resultValue = num1 >> num2;
+      break;
+    case "<":
+      resultValue = num1 << num2;
+      break;
+    default:
+      res.value = "Error";
+      return;
+  }
+
+  res.value = decimalToBinary(resultValue);
 }
 
 function decimalToBinary(num) {
-    let binary = [];
-    while (num > 0) {
-        binary.push(num % 2);
-        num = Math.floor(num / 2);
-    }
-    return binary.reverse().join('');
+  let binary = [];
+  while (num > 0) {
+    binary.push(num % 2);
+    num = Math.floor(num / 2);
+  }
+  return binary.reverse().join("");
 }
-
-function binaryToDecimal(binary) {
-    let decimal = 0;
-    for (let i = 0; i < binary.length; i++) {
-        decimal += binary[i] * Math.pow(2, binary.length - i - 1);
-    }
-    return decimal;
-}
-
-// register a keystroke listener
-document.addEventListener('keydown', event => {
-    if (event.code === "Numpad0" || event.code === "Digit0") {
-        insert(0);
-    } else if (event.code === "Numpad1" || event.code === "Digit1") {
-        insert(1);
-    } else if (event.code == "NumpadAdd" || event.key == "+") {
-        insert('+');
-    } else if (event.code == "NumpadSubtract" || event.key == "-") {
-        insert('-');
-    } else if (event.code == "NumpadDivide" || event.key == "/") {
-        insert('/');
-    } else if (event.code == "NumpadMultiply" || event.key == "*") {
-        insert('*');
-    } else if (event.code == "Enter" || event.key == "Enter") {
-        eql();
-    } else if (event.code == "KeyC" || event.key == "c" || event.code == "Backspace" || event.key == "Backspace") {
-        clc();
-    }
-});
