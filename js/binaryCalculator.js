e2590d05febd04ae5509e0cbb04672a304048fe8
Bharath-KumarReddy <127650446+Bharath-KumarReddy@users.noreply.github.com>
2023-10-02 10:57:15 +0530

    Update binaryCalculator.js

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -1,69 +1,67 @@
-const res = document.form.textview.value;
+const res = document.form.textview;
 
 function clc() {
-    document.form.textview.value = "";
+    res.value = "";
 }
 
 function insert(num) {
-    document.form.textview.value += num;
-}
-
-function logicalAnd(num1, num2) {
-    return num1 & num2;
-}
-
-function logicalOr(num1, num2) {
-    return num1 | num2;
-}
-
-function logicalXor(num1, num2) {
-    return num1 ^ num2;
+    res.value += num;
 }
 
 function eql() {
-    let result = document.form.textview.value;
+    let result = res.value;
 
     
-    const numbers = result.split(/[-+*/&|^]/);
+    const parts = result.split(/([\+\-\*\/&|^])/).filter(part => part.trim() !== '');
 
-    if (numbers.length !== 2) {
-        document.form.textview.value = "Error"; 
+    if (parts.length !== 3) {
+        res.value = "Error"; 
         return;
     }
 
-    const num1 = parseInt(numbers[0], 2);
-    const num2 = parseInt(numbers[1], 2);
+    const num1 = parseInt(parts[0], 2);
+    const operator = parts[1];
+    const num2 = parseInt(parts[2], 2);
 
     if (isNaN(num1) || isNaN(num2)) {
-        document.form.textview.value = "Error"; 
+        res.value = "Error"; 
         return;
     }
-
-    const operator = result.match(/[&|^]/)[0]; 
 
     let resultValue;
 
     switch (operator) {
+        case '+':
+            resultValue = num1 + num2;
+            break;
+        case '-':
+            resultValue = num1 - num2;
+            break;
+        case '*':
+            resultValue = num1 * num2;
+            break;
+        case '/':
+            if (num2 === 0) {
+                res.value = "Error"; 
+                return;
+            }
+            resultValue = num1 / num2;
+            break;
         case '&':
-            resultValue = logicalAnd(num1, num2); 
+            resultValue = num1 & num2; 
             break;
         case '|':
-            resultValue = logicalOr(num1, num2);
+            resultValue = num1 | num2; 
             break;
         case '^':
-            resultValue = logicalXor(num1, num2); 
+            resultValue = num1 ^ num2; 
             break;
         default:
-            resultValue = "Error please Enter correctly"; 
-            break;
+            res.value = "Error"; 
+            return;
     }
 
-
-    if (resultValue === 0) {
-        document.form.textview.value = "0";
-    } else {
-        document.form.textview.value = decimalToBinary(resultValue);
-    }
+    res.value = decimalToBinary(resultValue);
 }
 
 function decimalToBinary(num) {
@@ -74,30 +72,3 @@ function decimalToBinary(num) {
     }
     return binary.reverse().join('');
 }
-
-function binaryToDecimal(binary) {
-    let decimal = 0;
-    for (let i = 0; i < binary.length; i++) {
-        decimal += binary[i] * Math.pow(2, binary.length - i - 1);
-    }
-    return decimal;
-}
-
-// register a keystroke listener
-document.addEventListener('keydown', event => {
-    if (event.code === "Numpad0" || event.code === "Digit0") {
-        insert(0);
-    } else if (event.code === "Numpad1" || event.code === "Digit1") {
-        insert(1);
-    } else if (event.code == "NumpadAdd" || event.key == "+") {
-        insert('+');
-    } else if (event.code == "NumpadSubtract" || event.key == "-") {
-        insert('-');
-    } else if (event.code == "NumpadDivide" || event.key == "/") {
-        insert('/');
-    } else if (event.code == "NumpadMultiply" || event.key == "*") {
-        insert('*');
-    } else if (event.code == "KeyC" || event.key == "c" || event.code == "Backspace" || event.key == "Backspace") {
-        clc();
-    }
-});
