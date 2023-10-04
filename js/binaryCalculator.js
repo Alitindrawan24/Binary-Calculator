c2ca59676ed17a90afb526d43bf5f32448172906
Pedro Rafael Lázaro Nevado <119618710+Pedrop19@users.noreply.github.com>
2023-10-04 22:49:39 +0200

    Actualizar binaryCalculator.js

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -1,11 +1,12 @@
 const res = document.getElementById("res");
-const memory = { value: 0 };
+let memory = 0;
+let isMemorySet = false;
 
 function backspace() {
-    if (res.value === "Invalid Input" || res.value === "Error" || res.value === "Undefined") {
+    if (isError(res.value)) {
         res.value = "";
     } else {
-        res.value = res.value.slice(0, -1); // Simplified the string removal.
+        res.value = res.value.slice(0, -1);
     }
 }
 
@@ -14,7 +15,7 @@ function clc() {
 }
 
 function insert(num) {
-    if (res.value === "Invalid Input" || res.value === "Error" || res.value === "Undefined") {
+    if (isError(res.value)) {
         res.value = "";
     }
     res.value += num;
@@ -22,115 +23,189 @@ function insert(num) {
 
 function eql() {
     const result = res.value;
-    const parts = result.match(/([+\-*/&|^><])|(\d+)/g); // Improved splitting of expression.
+    const parts = result.split(/([\+\-\*\/&|^><])/).filter((part) => part.trim() !== "");
 
-    if (parts && parts.length === 3) {
-        const num1 = parseInt(parts[0], 2);
-        const operator = parts[1];
-        const num2 = parseInt(parts[2], 2);
-
-        if (!isNaN(num1) && !isNaN(num2)) {
-            let resultValue;
-
-            switch (operator) {
-                case "+":
-                    resultValue = num1 + num2;
-                    break;
-                case "-":
-                    resultValue = num1 - num2;
-                    break;
-                case "*":
-                    resultValue = num1 * num2;
-                    break;
-                case "/":
-                    if (num2 === 0) {
-                        res.value = "Undefined";
-                        return;
-                    }
-                    resultValue = (num1 / num2).toString(2); // Simplified binary conversion.
-                    break;
-                case ">":
-                    resultValue = num1 >> num2;
-                    break;
-                case "<":
-                    resultValue = num1 << num2;
-                    break;
-                default:
-                    res.value = "Error";
-                    return;
-            }
-
-            res.value = Math.abs(resultValue).toString(2); // Avoided duplication.
-        } else {
-            res.value = "Invalid Input";
-        }
-    } else {
+    if (parts.length !== 3) {
         res.value = "Invalid Input";
+        return;
+    }
+
+    const num1 = parseInt(parts[0], 2);
+    const operator = parts[1];
+    const num2 = parseInt(parts[2], 2);
+
+    if (isNaN(num1) || isNaN(num2)) {
+        res.value = "Invalid Input";
+        return;
+    }
+
+    let resultValue;
+
+    switch (operator) {
+        case "+":
+            resultValue = num1 + num2;
+            break;
+        case "-":
+            resultValue = num1 - num2;
+            break;
+        case "*":
+            resultValue = num1 * num2;
+            break;
+        case "/":
+            if (num2 === 0) {
+                res.value = "Undefined";
+                return;
+            }
+            var div = num1 / num2;
+            resultValue = div.toString(2);
+            break;
+        case ">":
+            resultValue = num1 >> num2;
+            break;
+        case "<":
+            resultValue = num1 << num2;
+            break;
+        default:
+            res.value = "Error";
+            return;
+    }
+    if (resultValue < 0) {
+        resultValue *= -1;
+        res.value = "-" + decimalToBinary(resultValue);
+    } else {
+        res.value = decimalToBinary(resultValue);
     }
 }
 
-function toggleComplement(complementType) {
-    const binary = res.value;
-    const complemented = binary
+function decimalToBinary(num) {
+    let binary = [];
+    if (num == 0) binary.push(0);
+    while (num > 0) {
+        binary.push(num % 2);
+        num = Math.floor(num / 2);
+    }
+    return binary.reverse().join("");
+}
+
+function oneComplement() {
+    const term = res.value;
+    const onesComplement = term
         .split("")
         .map((bit) => (bit === "0" ? "1" : "0"))
         .join("");
+    res.value = onesComplement;
+}
 
-    if (complementType === "twos") {
-        const decimalValue = parseInt(complemented, 2);
-        const twosComplementDecimal = decimalValue + 1;
-        const twosComplementBinary = twosComplementDecimal.toString(2);
-        res.value = twosComplementBinary.padStart(binary.length, "0"); // Simplified padding.
-    } else {
-        res.value = complemented;
+function twoComplement() {
+    const binary = res.value;
+    const onesComplement = binary
+        .split("")
+        .map((bit) => (bit === "0" ? "1" : "0"))
+        .join("");
+    const decimalValue = parseInt(onesComplement, 2);
+    const twosComplementDecimal = decimalValue + 1;
+    const twosComplementBinary = twosComplementDecimal.toString(2);
+    const paddedTwosComplement = twosComplementBinary.padStart(
+        binary.length,
+        "0"
+    );
+    res.value = paddedTwosComplement;
+}
+
+function binaryToDecimal(binary) {
+    let decimal = 0;
+    for (let i = 0; i < binary.length; i++) {
+        decimal += binary[i] * Math.pow(2, binary.length - i - 1);
     }
+    return decimal;
 }
 
 function addToMemory() {
     if (!isNaN(parseFloat(res.value))) {
-        memory.value += parseFloat(res.value); // Utilized memory object.
+        memory += parseFloat(res.value);
+        isMemorySet = true;
     }
 }
 
 function subtractFromMemory() {
     if (!isNaN(parseFloat(res.value))) {
-        memory.value -= parseFloat(res.value); // Utilized memory object.
+        memory -= parseFloat(res.value);
+        isMemorySet = true;
     }
 }
 
 function recallMemory() {
-    res.value = memory.value;
+    if (isMemorySet) {
+        res.value = memory;
+    }
 }
 
 function clearMemory() {
-    memory.value = 0;
+    memory = 0;
+    isMemorySet = false;
 }
 
 document.addEventListener("keydown", (event) => {
-    // Simplified event handling using key names.
-    if (event.key === "0" || event.key === "1") {
-        insert(event.key);
-    } else if (event.key === "+" || event.key === "-" || event.key === "*" || event.key === "/") {
-        insert(event.key);
-    } else if (event.key === "Enter") {
+    if (
+        event.code === "Numpad0" ||
+        event.code === "Digit0" ||
+        event.code === "Numpad1" ||
+        event.code === "Digit1"
+    ) {
+        event.preventDefault();
+    }
+
+    if (event.code === "Numpad0" || event.code === "Digit0") {
+        insert(0);
+    } else if (event.code === "Numpad1" || event.code === "Digit1") {
+        insert(1);
+    } else if (event.code == "NumpadAdd" || event.key == "+") {
+        insert("+");
+    } else if (event.code == "NumpadSubtract" || event.key == "-") {
+        insert("-");
+    } else if (event.code == "NumpadDivide" || event.key == "/") {
+        insert("/");
+    } else if (event.code == "NumpadMultiply" || event.key == "*") {
+        insert("*");
+    } else if (event.code == "Enter" || event.key == "Enter") {
         eql();
-    } else if (event.key === "Backspace") {
-        backspace();
-    } else if (event.key === "c" || event.key === "C") {
+    } else if (
+        event.code == "KeyC" ||
+        event.key == "c" ||
+        event.code == "Backspace" ||
+        event.key == "Backspace"
+    ) {
         clc();
-    } else if (event.key === "m" || event.key === "M") {
+    } else if (event.code === "KeyM" || event.key === "m") {
         addToMemory();
-    } else if (event.key === "n" || event.key === "N") {
+    } else if (event.code === "KeyN" || event.key === "n") {
         subtractFromMemory();
-    } else if (event.key === "r" || event.key === "R") {
+    } else if (event.code === "KeyR" || event.key === "r") {
         recallMemory();
-    } else if (event.key === "l" || event.key === "L") {
+    } else if (event.code === "KeyL" || event.key === "l") {
         clearMemory();
     }
 });
 
+const toggleThemeButton = document.getElementById("toggle-theme-button");
+const theme1Link = document.getElementById("theme1");
+const theme2Link = document.getElementById("theme2");
+let isTheme1Active = true;
+
+toggleThemeButton.addEventListener("click", () => {
+    if (isTheme1Active) {
+        theme1Link.disabled = true;
+        theme2Link.disabled = false;
+    } else {
+        theme1Link.disabled = false;
+        theme2Link.disabled = true;
+    }
+    isTheme1Active = !isTheme1Active;
+});
+
 res.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-        eql();
+        event.preventDefault();
+        document.getElementById("eql").click();
     }
 });
